83fb07e9cc7cd59c0494834125306cec0d092d9e
Ben Morrall <bemo56@hotmail.com>
2013-03-14 23:04:41 +1100

    Scaffold Controller Models don't use namespace of Controller

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -2,6 +2,10 @@
 require_dependency "<%= namespaced_file_path %>/application_controller"
 
 <% end -%>
+<%- singular_var_name = singular_table_name.split('_')[-1] -%>
+<%- plural_var_name = plural_table_name.split('_')[-1] -%>
+<%- local_class_name = class_name.split("::")[-1] -%>
+<%- orm_instance = Rails::Generators::ActiveModel.new singular_var_name -%>
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < ApplicationController
   load_and_authorize_resource
@@ -9,50 +13,50 @@ class <%= controller_class_name %>Controller < ApplicationController
   # GET <%= route_url %>
   # GET <%= route_url %>.json
   def index
-    # @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+    # @<%= plural_var_name %> = <%= orm_class.all(local_class_name) %>
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render <%= key_value :json, "@#{plural_table_name}" %> }
+      format.json { render <%= key_value :json, "@#{plural_var_name}" %> }
     end
   end
 
   # GET <%= route_url %>/1
   # GET <%= route_url %>/1.json
   def show
-    # @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    # @<%= singular_var_name %> = <%= orm_class.find(local_class_name, "params[:id]") %>
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render <%= key_value :json, "@#{singular_table_name}" %> }
+      format.json { render <%= key_value :json, "@#{singular_var_name}" %> }
     end
   end
 
   # GET <%= route_url %>/new
   # GET <%= route_url %>/new.json
   def new
-    # @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+    # @<%= singular_var_name %> = <%= orm_class.build(local_class_name) %>
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render <%= key_value :json, "@#{singular_table_name}" %> }
+      format.json { render <%= key_value :json, "@#{singular_var_name}" %> }
     end
   end
 
   # GET <%= route_url %>/1/edit
   def edit
-    # @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    # @<%= singular_var_name %> = <%= orm_class.find(local_class_name, "params[:id]") %>
   end
 
   # POST <%= route_url %>
   # POST <%= route_url %>.json
   def create
-    # @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
+    # @<%= singular_var_name %> = <%= orm_class.build(local_class_name, "params[:#{singular_var_name}]") %>
 
     respond_to do |format|
       if @<%= orm_instance.save %>
-        format.html { redirect_to @<%= singular_table_name %>, <%= key_value :notice, "'#{human_name} was successfully created.'" %> }
-        format.json { render <%= key_value :json, "@#{singular_table_name}" %>, <%= key_value :status, ':created' %>, <%= key_value :location, "@#{singular_table_name}" %> }
+        format.html { redirect_to <%= singular_table_name %>_path(@<%= singular_var_name %>), <%= key_value :notice, "'#{human_name} was successfully created.'" %> }
+        format.json { render <%= key_value :json, "@#{singular_var_name}" %>, <%= key_value :status, ':created' %>, <%= key_value :location, "#{singular_table_name}_path(@#{singular_var_name})" %> }
       else
         format.html { render <%= key_value :action, '"new"' %> }
         format.json { render <%= key_value :json, "@#{orm_instance.errors}" %>, <%= key_value :status, ':unprocessable_entity' %> }
@@ -63,11 +67,11 @@ class <%= controller_class_name %>Controller < ApplicationController
   # PUT <%= route_url %>/1
   # PUT <%= route_url %>/1.json
   def update
-    # @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    # @<%= singular_var_name %> = <%= orm_class.find(local_class_name, "params[:id]") %>
 
     respond_to do |format|
-      if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
-        format.html { redirect_to @<%= singular_table_name %>, <%= key_value :notice, "'#{human_name} was successfully updated.'" %> }
+      if @<%= orm_instance.update_attributes("params[:#{singular_var_name}]") %>
+        format.html { redirect_to <%= singular_table_name %>_path(@<%= singular_var_name %>), <%= key_value :notice, "'#{human_name} was successfully updated.'" %> }
         format.json { head :no_content }
       else
         format.html { render <%= key_value :action, '"edit"' %> }
@@ -79,7 +83,7 @@ class <%= controller_class_name %>Controller < ApplicationController
   # DELETE <%= route_url %>/1
   # DELETE <%= route_url %>/1.json
   def destroy
-    # @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    # @<%= singular_var_name %> = <%= orm_class.find(local_class_name, "params[:id]") %>
     @<%= orm_instance.destroy %>
 
     respond_to do |format|
