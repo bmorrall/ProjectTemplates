62c92d5814f06836752d12e08e671555de1e6027
Ben Morrall <bemo56@hotmail.com>
2013-03-16 22:57:26 +1100

    Added coverage for admin row classes

--- lib/templates/rspec/scaffold/index_spec.rb
+++ lib/templates/rspec/scaffold/index_spec.rb
@@ -4,37 +4,54 @@ require 'spec_helper'
 <%- output_attributes = attributes.reject{|attribute| [:timestamp].index(attribute.type) } -%>
 describe "<%= ns_table_name %>/index" do
   before(:each) do
-    assign(:<%= file_name.pluralize %>, [
 <% [1,2].each_with_index do |id, model_index| -%>
-      FactoryGirl.build_stubbed(:<%= file_name %><%= output_attributes.empty? ? (model_index == 1 ? ')' : '),') : ',' %>
+    @<%= file_name %>_<%= model_index + 1 %> = FactoryGirl.build_stubbed(:<%= file_name %><%= output_attributes.empty? ? ')' : ',' %>
 <% output_attributes.each_with_index do |attribute, attribute_index| -%>
-  <%-
-  attribute_prefix = ''
-  attribute_suffix = ')'
-  if attribute.type == :datetime
-    attribute_value = value_for(attribute)
-    attribute_prefix = 'DateTime.parse('
-  elsif attribute.type == :time
-    attribute_value = value_for(attribute).to_time.strftime('%T').dump
-    attribute_prefix = 'Time.parse('
-  elsif attribute.type == :date
-    attribute_value = value_for(attribute)
-    attribute_prefix = 'Date.parse('
-  else
-    attribute_value = value_for(attribute)
-    attribute_suffix = ''
-  end
-  -%>
-        :<%= attribute.name %> => <%= attribute_prefix %><%= attribute_value %><%= attribute_suffix %><%= attribute_index == output_attributes.length - 1 ? '' : ','%>
+<%-
+attribute_prefix = ''
+attribute_suffix = ')'
+if attribute.type == :datetime
+  attribute_value = value_for(attribute)
+  attribute_prefix = 'DateTime.parse('
+elsif attribute.type == :time
+  attribute_value = value_for(attribute).to_time.strftime('%T').dump
+  attribute_prefix = 'Time.parse('
+elsif attribute.type == :date
+  attribute_value = value_for(attribute)
+  attribute_prefix = 'Date.parse('
+else
+  attribute_value = value_for(attribute)
+  attribute_suffix = ''
+end
+-%>
+      :<%= attribute.name %> => <%= attribute_prefix %><%= attribute_value %><%= attribute_suffix %><%= attribute_index == output_attributes.length - 1 ? '' : ','%>
 <% end -%>
 <% if !output_attributes.empty? -%>
-      <%= model_index == 1 ? ')' : '),' %>
+    )
 <% end -%>
+<% end -%>
+    assign(:<%= file_name.pluralize %>, [
+<% [1,2].each_with_index do |id, model_index| -%>
+      @<%= file_name %>_<%= model_index + 1 %><%= model_index == 1 ? '' : ',' %>
 <% end -%>
     ])
     @ability = Object.new
     @ability.extend(CanCan::Ability)
     controller.stub(:current_ability) { @ability }
+  end
+
+  it 'renders a table rows with <%= file_name %> classes' do
+    render
+<% unless webrat? -%>
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+<% end -%>
+<% [1,2].each_with_index do |id, model_index| -%>
+<% if webrat? -%>
+    rendered.should have_selector("table>tr.<%= file_name %>_#{@<%= file_name %>_<%= model_index + 1 %>.id}", :count => 1)
+<% else -%>
+    assert_select "table>tr.<%= file_name %>_#{@<%= file_name %>_<%= model_index + 1 %>.id}", :count => 1
+<% end -%>
+<% end -%>
   end
 
   it "renders a list of <%= file_name.pluralize %>" do
